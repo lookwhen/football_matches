29d7fe5230b764b7375138877a78c43bcd0e47d6
Jan Jarzębinski <janjarzebinski123@gmail.com>
2026-09-02 17:38:47 +0200

    Added getMatches

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-const matches = []
+async function getMatches(){
+    const file = "./worldcup.json";
+
+    try{
+        const response = await fetch(file);
+
+        if(!response.ok){
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        const result = await response.json();
+        console.log(result);
+    }
+    catch(error){
+        console.error(error.message);
+    }
+}
